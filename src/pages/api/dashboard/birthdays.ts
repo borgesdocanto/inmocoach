dc4e4c4394171341f85ef6d166341c18098a7450
Leandro Borges <leandroborges@hotmail.com>
2026-05-18 15:01:39 +0000

    fix: authOptions desde lib/auth no desde nextauth route

--- src/pages/api/dashboard/birthdays.ts
+++ src/pages/api/dashboard/birthdays.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]";
+import { authOptions } from "../../lib/auth";
 import { supabaseAdmin } from "../../lib/supabase";
 import { getEffectiveEmail } from "../../lib/impersonation";
 
